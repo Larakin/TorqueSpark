9d7ee55e7c2c1aa6343452eaa8e77e68bdbdaa6b
Justin Erickson <Larakinsystems@gmail.com>
2014-07-16 22:53:03 -0600

    Add all Files

---
+++ app/src/main/java/com/torquedout/torquespark/cloud/api/ListDevicesResponse.java
@@ -0,0 +1,18 @@
+package com.torquedout.torquespark.cloud.api;
+
+import java.util.List;
+
+
+
+public class ListDevicesResponse extends SimpleResponse {
+
+
+	public final List<Device> devices;
+
+
+	public ListDevicesResponse(boolean ok, List<String> errors, List<Device> devices) {
+		super(ok, errors);
+		this.devices = devices;
+	}
+
+}
---
+++ app/src/main/java/com/torquedout/torquespark/spark/PinAction.java
@@ -0,0 +1,10 @@
+package com.torquedout.torquespark.spark;
+
+public enum PinAction {
+
+	ANALOG_READ,
+	ANALOG_WRITE,
+	DIGITAL_READ,
+	DIGITAL_WRITE,
+	NONE;
+}
